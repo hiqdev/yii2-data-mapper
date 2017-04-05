688ff4adf1a086d9119445f6209c2bc2f05f544c
Andrii Vasyliev <sol@hiqdev.com>
2017-04-05 12:11:04 +0200

    inited

---
+++ README.md
@@ -0,0 +1,37 @@
+# HiAPI Core
+
+**HiAPI - template for Yii2 based API**
+
+[![Latest Stable Version](https://poser.pugx.org/hiqdev/hiapi-core/v/stable)](https://packagist.org/packages/hiqdev/hiapi-core)
+[![Total Downloads](https://poser.pugx.org/hiqdev/hiapi-core/downloads)](https://packagist.org/packages/hiqdev/hiapi-core)
+[![Build Status](https://img.shields.io/travis/hiqdev/hiapi-core.svg)](https://travis-ci.org/hiqdev/hiapi-core)
+[![Scrutinizer Code Coverage](https://img.shields.io/scrutinizer/coverage/g/hiqdev/hiapi-core.svg)](https://scrutinizer-ci.com/g/hiqdev/hiapi-core/)
+[![Scrutinizer Code Quality](https://img.shields.io/scrutinizer/g/hiqdev/hiapi-core.svg)](https://scrutinizer-ci.com/g/hiqdev/hiapi-core/)
+[![Dependency Status](https://www.versioneye.com/php/hiqdev:hiapi-core/dev-master/badge.svg)](https://www.versioneye.com/php/hiqdev:hiapi-core/dev-master)
+
+HiAPI is a template for creating Yii2 based API.
+
+## Installation
+
+The preferred way to install this yii2-extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```sh
+php composer.phar require "hiqdev/hiapi-core"
+```
+
+or add
+
+```json
+"hiqdev/hiapi-core": "*"
+```
+
+to the require section of your composer.json.
+
+## License
+
+This project is released under the terms of the BSD-3-Clause [license](LICENSE).
+Read more [here](http://choosealicense.com/licenses/bsd-3-clause).
+
+Copyright © 2017, HiQDev (http://hiqdev.com/)
---
+++ src/config/hiapi.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * HiAPI - template for Yii2 based API
+ *
+ * @link      https://github.com/hiqdev/hiapi-core
+ * @package   hiapi-core
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
+
+return array_filter([
+    'id' => 'hiapi',
+    'name' => 'HiApi',
+    'basePath' => dirname(__DIR__),
+    'viewPath' => '@hiapi/views',
+    'vendorPath' => '@root/vendor',
+    'runtimePath' => '@root/runtime',
+    'controllerNamespace' => 'hiapi\controllers',
+    'bootstrap' => array_filter([
+        'log' => 'log',
+        'debug' => empty($params['debug.enabled']) ? null : 'debug',
+    ]),
+    'components' => [
+        'request' => [
+            'enableCsrfCookie' => false,
+        ],
+        'mailer' => [
+            'viewPath' => '@hiapi/views/mail',
+            'htmlLayout' => '@hiapi/views/layouts/mail-html',
+            'textLayout' => '@hiapi/views/layouts/mail-text',
+        ],
+        'log' => [
+            'traceLevel' => defined('YII_DEBUG') && YII_DEBUG ? 3 : 0,
+            'targets' => [
+                'default' => [
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'class' => \yii\web\UrlManager::class,
+            'catchAll' => ['hiapi/catch'],
+        ],
+    ],
+    'modules' => array_filter([
+        'debug' => empty($params['debug.enabled']) ? null : array_filter([
+            'class' => \yii\debug\Module::class,
+            'allowedIPs' => isset($params['debug.allowedIps']) ? $params['debug.allowedIps'] : null,
+            'historySize' => isset($params['debug.historySize']) ? $params['debug.historySize'] : null,
+        ]),
+    ]),
+    'container' => [
+        'singletons' => [
+            \yii\base\Application::class => function () {
+                return Yii::$app;
+            },
+            \yii\mail\MailerInterface::class => function () {
+                return Yii::$app->get('mailer');
+            },
+        ],
+    ],
+]);
