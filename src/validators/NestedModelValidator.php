1f5fd8f3b16f168fcba953fb3473aa73391cd80d
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-06-06 16:52:56 +0300

    Updated NestedModelValidator to support model creation

--- src/validators/NestedModelValidator.php
+++ src/validators/NestedModelValidator.php
@@ -2,12 +2,49 @@
 
 namespace hiapi\validators;
 
+use yii\base\InvalidConfigException;
 use yii\base\InvalidParamException;
 use yii\base\Model;
 use yii\validators\Validator;
 
 class NestedModelValidator extends Validator
 {
+    public $modelClass;
+
+    public function init()
+    {
+        if (!isset($this->modelClass)) {
+            throw new InvalidConfigException('Property "modelClass" is missing');
+        }
+
+        parent::init();
+    }
+
+    public function validateAttribute($model, $attribute)
+    {
+        $oldValue = $model->$attribute;
+
+        $newModel = $this->createModel($model->$attribute);
+        $model->$attribute = $newModel;
+
+        $validationResult = parent::validateAttribute($model, $attribute);
+        if ($validationResult === null) {
+            return null;
+        }
+
+        $model->$attribute = $oldValue;
+
+        return $validationResult;
+    }
+
+    private function createModel($data)
+    {
+        $model = new $this->modelClass;
+        $model->load($data, '');
+
+        return $model;
+    }
+
     /**
      * @param Model $model
      * @return array|null
