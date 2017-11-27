56e2b4539735f1f8847b5c0c41fc07bde53d06dc
Andrii Vasyliev <sol@hiqdev.com>
2017-11-27 15:39:18 +0000

    added DB connection to EntityManager, added interface

--- src/components/EntityManager.php
+++ src/components/EntityManager.php
@@ -12,10 +12,41 @@ namespace hiapi\components;
 
 use Yii;
 
-class EntityManager extends \yii\base\Component
+class EntityManager extends \yii\base\Component implements EntityManagerInterface
 {
+    /**
+     * @var array
+     */
     public $repositories = [];
 
+    /**
+     * @var ConnectionInterface
+     */
+    public $db;
+
+    public function __construct(
+        ConnectionInterface $db,
+        array $config = []
+    ) {
+        $this->db = $db;
+
+        parent::__construct($config);
+    }
+
+    /**
+     * Get database connection.
+     * @return ConnectionInterface
+     */
+    public function getConnection()
+    {
+        return $this->db;
+    }
+
+    /**
+     * Get entity repository by entity or class.
+     * @param object|string $entityClass entity or class
+     * @return RepositoryInterface
+     */
     public function getRepository($entityClass)
     {
         if (is_object($entityClass)) {
@@ -31,5 +62,37 @@ class EntityManager extends \yii\base\Component
         }
 
         return $this->repositories[$entityClass];
+    }
+
+    /**
+     * Save given entity into it's repository.
+     * @param object $entity
+     */
+    public function save($entity)
+    {
+        $repo = $this->getRepository($entity);
+        $repo->save($entity);
+    }
+
+    /**
+     * Create entity of given class with given data.
+     * @param string $entityClass
+     * @param array $data
+     * @return object
+     */
+    public function create($entityClass, array $data)
+    {
+        return $this->getRepository($entityClass)->create($data);
+    }
+
+    /**
+     * XXX TODO think of the whole process:
+     * alternative: find and populate whole entity
+     * @param object $entity 
+     * @return string|int
+     */
+    public function findId($entity)
+    {
+        return $this->getRepository($entity)->findId($entity);
     }
 }
