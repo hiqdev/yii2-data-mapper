59e4bb4332a9e2700620da1f70f181874f103a63
Andrii Vasyliev <sol@hiqdev.com>
2018-05-07 10:42:36 +0000

    added `GeneratedHydrator` class (to be used instead of trait)

---
+++ src/hydrator/GeneratedHydrator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Data Mapper for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-data-mapper
+ * @package   yii2-data-mapper
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2018, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\yii\DataMapper\hydrator;
+
+use Zend\Hydrator\HydratorInterface;
+
+abstract class GeneratedHydrator implements HydratorInterface
+{
+    use RootHydratorAwareTrait;
+    use GeneratedHydratorTrait;
+}
