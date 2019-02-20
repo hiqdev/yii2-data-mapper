7fb52cda62387b059e354a01183d6b3f38de38db
Andrii Vasyliev <sol@hiqdev.com>
2019-02-20 22:19:38 +0200

    Added hydrator `NotConfiguredException`

---
+++ src/hydrator/NotConfiguredException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Data Mapper for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-data-mapper
+ * @package   yii2-data-mapper
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017-2018, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\yii\DataMapper\hydrator;
+
+/**
+ * Class NotConfiguredException
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+class NotConfiguredException extends \Exception
+{
+}
